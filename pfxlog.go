c2d20500237581b07eeef10d383107212d99e82f
Michael Quigley <michael@quigley.com>
2018-05-22 22:27:28 -0400

    Initial import.

---
+++ pfxlog.go
@@ -0,0 +1,28 @@
+package pfxlog
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"github.com/x-cray/logrus-prefixed-formatter"
+	"runtime"
+)
+
+func Global(level logrus.Level) {
+	fmt := new(prefixed.TextFormatter)
+	logrus.SetFormatter(fmt)
+	logrus.SetLevel(level)
+}
+
+func Logger() *logrus.Entry {
+	return logrus.StandardLogger().WithField("prefix", functionName())
+}
+
+func AttachedLogger(attached string) *logrus.Entry {
+	return logrus.StandardLogger().WithField("prefix", fmt.Sprintf("%s:[%s]", functionName(), attached))
+}
+
+func functionName() string {
+	pc, _, _, _ := runtime.Caller(2)
+	f := runtime.FuncForPC(pc)
+	return f.Name()
+}
